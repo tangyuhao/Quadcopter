0cbafb67ae6a5446210e3c9107490c6677dec00c
tangyuhao <yhtang1993@126.com>
2015-05-24 00:01:44 +0800

    add some control_flags

--- control_flag/control.h
+++ control_flag/control.h
@@ -1,10 +1,12 @@
 #ifndef __CONTROL_H__
 #define __CONTROL_H__
-#define BEAGLEBONE 0
+#define BEAGLEBONE 1
 #define DEBUG 0
-#define CHAN_DEBUG 0
+#define CHAN_DEBUG 1
 #define WP_DEBUG 0
 #define ST_DEBUG 0
 #define STATUS_DEBUG 0
-
+#define SAFE_DOWN 0
+#define SAFE_CHAN12 0
+#define SAFE_HENCE 0
 #endif
